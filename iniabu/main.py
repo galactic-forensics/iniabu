eee7ca79e24efd54b2c5862e2183f2a2544fa318
Reto Trappitsch <reto@galactic-forensics.space>
2020-12-16 19:54:11 -0500

    Added the linear law for internal normalization
    Linear law is really ugly, but it works... Needs quite some explanation
    of why it should not be chosen in the documentation.

--- iniabu/main.py
+++ iniabu/main.py
@@ -566,7 +566,23 @@ class IniAbu(object):
         :rtype: float, ndarray(float)
 
         :raises ValueError: Input values are mismatched, selected law is not valid.
+
+        Example:
+            >>> from iniabu import ini
+            >>> # define input values
+            >>> norm_isos = ("Ni-58", "Ni-60") # normalizing isotopes
+            >>> sample_vals = (1., 0.4, 0.15, 0.5, 0.3)  # measured isotope abundances
+            >>> sample_norm_vals = (sample_vals[0], sample_vals[1])  # Ni-58, Ni-60
+            >>> # get the internally normalized values for all nickel isotopes
+            >>> ini.iso_int_norm("Ni", norm_isos, sample_vals, sample_norm_vals)
+            array([     0.        ,      0.        ,  75068.93030287,  77568.12815864,
+                   189333.87185102])
         """
+        # if nominator is an element, get all isos
+        if nominator in self.ele_dict.keys():
+            nominator = self._get_all_isos(nominator)
+
+        # values to numpy arrays
         sample_values = np.array(sample_values)
         sample_norm_values = np.array(sample_norm_values)
 
@@ -580,6 +596,14 @@ class IniAbu(object):
         # get masses and ratios for nominator isotope
         mass_nominator = self.iso[nominator].mass
         iso_ratio_solar = self.iso_ratio(nominator, norm_isos[0])
+
+        # check for input value shape mismatch
+        if mass_nominator.shape != sample_values.shape and mass_nominator.shape != ():
+            raise ValueError(
+                "Length of requested isotope ratios does not match length of "
+                "provided values."
+            )
+
         if law == "exp":
             # calculate exponent for exponential law
             beta = np.log10(iso_ratio_smpl_norm / iso_ratio_solar_norm) / np.log10(
@@ -591,18 +615,47 @@ class IniAbu(object):
                 / sample_norm_values[0]
                 / (mass_nominator / mass_maj_norm) ** beta
             )
+            # capital delta value
+            capd_value = (smp_ratio / iso_ratio_solar - 1) * delta_factor
         elif law == "lin":
-            raise NotImplementedError
+            # get the respective (lower-case) delta values
+            delta_norm = self.iso_delta(
+                norm_isos[1],
+                norm_isos[0],
+                sample_norm_values[1] / sample_norm_values[0],
+                delta_factor=delta_factor,
+            )
+            delta_samp = self.iso_delta(
+                nominator,
+                norm_isos[0],
+                sample_values / sample_norm_values[0],
+                delta_factor=delta_factor,
+            )
+            # calculate mass difference ratio
+            corr_factor = (mass_maj_norm - mass_nominator) / (
+                mass_maj_norm - mass_min_norm
+            )
+            # calculate capital delta value
+            capd_value = delta_samp - corr_factor * delta_norm
+
+            capd_value = np.array(capd_value)
+
+            # make sure the value makes sense - the rest is up to the user
+            # this is kind of ugly but does the trick for now...
+            if capd_value.shape == ():
+                if capd_value < -delta_factor:
+                    capd_value = -delta_factor
+            else:
+                for it, val in enumerate(capd_value):
+                    if val < -delta_factor:
+                        capd_value[it] = -delta_factor
         else:
             raise ValueError(
                 f"The selected law {law} is invalid. Please select either 'exp' for "
                 f"an exponential law or 'lin' for a linear law."
             )
 
-        # calculate capital delta value
-        eps_value = (smp_ratio / iso_ratio_solar - 1) * delta_factor
-
-        return eps_value
+        return return_as_ndarray(capd_value)
 
     def ele_ratio(self, nominator, denominator, mass_fraction=None):
         """Get the ratios of given elements.
--- tests/test_main.py
+++ tests/test_main.py
@@ -307,8 +307,8 @@ def test_iso_delta_many_values(ini_default):
 # INTERNAL NORMALIZATION #
 
 
-# fixme insert lin, log both selected with parametrization of pytest
-def test_iso_int_norm_zeros(ini_default):
+@pytest.mark.parametrize("law", ("exp", "lin"))
+def test_iso_int_norm_zeros(ini_default, law):
     """Normalization isotopes return zero for internal normalization."""
     isos = ("Ni-58", "Ni-62")
     norm_isos = isos
@@ -316,7 +316,7 @@ def test_iso_int_norm_zeros(ini_default):
     norm_values = smp_values
 
     # do internal normalization
-    retval = ini_default.iso_int_norm(isos, norm_isos, smp_values, norm_values)
+    retval = ini_default.iso_int_norm(isos, norm_isos, smp_values, norm_values, law=law)
 
     assert retval == pytest.approx(0, abs=1e-6)
 
@@ -352,6 +352,48 @@ def test_iso_int_norm_exp_single(ini_default, smp_value, smp_norm_values):
 
     retval_gotten = ini_default.iso_int_norm(
         nominator_iso, norm_isos, smp_value, smp_norm_values
+    )
+    assert retval_gotten == pytest.approx(retval_expected)
+
+
+@given(
+    smp_value=st.floats(min_value=0, allow_infinity=False),
+    smp_norm_values=st.tuples(
+        st.floats(min_value=1e-50, max_value=1e50),
+        st.floats(min_value=1e-50, max_value=1e50),
+    ),
+)
+def test_iso_int_norm_lin_single(ini_default, smp_value, smp_norm_values):
+    """Internal normalization using linear law for one isotope, one value."""
+    nominator_iso = "Ni-60"
+    norm_isos = ("Ni-62", "Ni-61")
+
+    # masses
+    mass_nominator = ini_default.iso[nominator_iso].mass
+    mass_norm_isos = ini_default.iso[norm_isos].mass
+
+    # delta values for the sample and normalization
+    delta_value_norm = ini_default.iso_delta(
+        norm_isos[1],
+        norm_isos[0],
+        smp_norm_values[1] / smp_norm_values[0],
+        delta_factor=10000,
+    )
+    delta_value_smp = ini_default.iso_delta(
+        nominator_iso, norm_isos[0], smp_value / smp_norm_values[0], delta_factor=10000
+    )
+    # mass correction factor
+    corr_fac = (mass_norm_isos[0] - mass_nominator) / (
+        mass_norm_isos[0] - mass_norm_isos[1]
+    )
+
+    retval_expected = delta_value_smp - corr_fac * delta_value_norm
+
+    if retval_expected < -10000:
+        retval_expected = -10000.0
+
+    retval_gotten = ini_default.iso_int_norm(
+        nominator_iso, norm_isos, smp_value, smp_norm_values, law="lin"
     )
     assert retval_gotten == pytest.approx(retval_expected)
 
@@ -400,19 +442,20 @@ def test_iso_int_norm_exp_single_delta(ini_default, delta_fct):
 )
 def test_iso_int_norm_exp_multi_isos(ini_default, smp_values):
     """Internal normalization using exponential law for multiple isotopes."""
-    nominator_iso = ini_default._get_all_isos("Ni")
+    nominator_ele = "Ni"
+    nominator_isos = ini_default._get_all_isos(nominator_ele)
     norm_isos_ind = (0, 3)
-    norm_isos = (nominator_iso[norm_isos_ind[0]], nominator_iso[norm_isos_ind[1]])
+    norm_isos = (nominator_isos[norm_isos_ind[0]], nominator_isos[norm_isos_ind[1]])
     smp_values = np.array(smp_values)
     smp_norm_values = np.array([smp_values[0], smp_values[3]])
 
     # masses
-    mass_nominator = ini_default.iso[nominator_iso].mass
+    mass_nominator = ini_default.iso[nominator_isos].mass
     mass_norm_isos = ini_default.iso[norm_isos].mass
 
     # isotope ratios solar system
     iso_ratio_solar_norm = ini_default.iso_ratio(norm_isos[1], norm_isos[0])
-    iso_ratio_solar = ini_default.iso_ratio(nominator_iso, norm_isos[0])
+    iso_ratio_solar = ini_default.iso_ratio(nominator_isos, norm_isos[0])
 
     # exponential law
     beta = np.log10(
@@ -424,7 +467,59 @@ def test_iso_int_norm_exp_multi_isos(ini_default, smp_values):
     retval_expected = (corrected_ratio / iso_ratio_solar - 1) * 10000
 
     retval_gotten = ini_default.iso_int_norm(
-        nominator_iso, norm_isos, smp_values, smp_norm_values
+        nominator_ele, norm_isos, smp_values, smp_norm_values
+    )
+    assert retval_gotten == pytest.approx(retval_expected)
+
+
+@given(
+    smp_values=st.tuples(
+        st.floats(min_value=1e-50, max_value=1e50),  # Ni-58
+        st.floats(min_value=0, max_value=1e50),  # Ni-60
+        st.floats(min_value=0, max_value=1e50),  # Ni-61
+        st.floats(min_value=1e-50, max_value=1e50),  # Ni-62
+        st.floats(min_value=0, max_value=1e50),  # Ni-64
+    )
+)
+def test_iso_int_norm_lin_multi(ini_default, smp_values):
+    """Internal normalization using linear law for multiple isotopes."""
+    nominator_ele = "Ni"
+    nominator_isos = ini_default._get_all_isos(nominator_ele)
+    norm_isos_ind = (0, 3)
+    norm_isos = (nominator_isos[norm_isos_ind[0]], nominator_isos[norm_isos_ind[1]])
+    smp_values = np.array(smp_values)
+    smp_norm_values = np.array([smp_values[0], smp_values[3]])
+
+    # masses
+    mass_nominator = ini_default.iso[nominator_isos].mass
+    mass_norm_isos = ini_default.iso[norm_isos].mass
+
+    # delta values for the sample and normalization
+    delta_value_norm = ini_default.iso_delta(
+        norm_isos[1],
+        norm_isos[0],
+        smp_norm_values[1] / smp_norm_values[0],
+        delta_factor=10000,
+    )
+    delta_value_smp = ini_default.iso_delta(
+        nominator_isos,
+        norm_isos[0],
+        smp_values / smp_norm_values[0],
+        delta_factor=10000,
+    )
+    # mass correction factor
+    corr_fac = (mass_norm_isos[0] - mass_nominator) / (
+        mass_norm_isos[0] - mass_norm_isos[1]
+    )
+
+    retval_expected = delta_value_smp - corr_fac * delta_value_norm
+
+    for it, val in enumerate(retval_expected):
+        if val < -10000:
+            retval_expected[it] = -10000.0
+
+    retval_gotten = ini_default.iso_int_norm(
+        nominator_ele, norm_isos, smp_values, smp_norm_values, law="lin"
     )
     assert retval_gotten == pytest.approx(retval_expected)
 
@@ -459,6 +554,28 @@ def test_iso_int_norm_exp_multi_values(ini_default):
         nominator_iso, norm_isos, smp_values, smp_norm_values
     )
     assert retval_gotten == pytest.approx(retval_expected)
+
+
+def test_iso_int_norm_input_mismatch(ini_default):
+    """Raise ValueError input values are mismatched."""
+    nominator_iso = "Ni"
+    norm_isos = ("Ni-58", "Ni-62")
+
+    # sample values - enough hypothesis already
+    smp_value = 1.0
+    smp_norm_values = np.array([10.0, 2.0])
+
+    bad_law = "tmp"
+
+    with pytest.raises(ValueError) as err_info:
+        _ = ini_default.iso_int_norm(
+            nominator_iso, norm_isos, smp_value, smp_norm_values, law=bad_law
+        )
+    err_msg = err_info.value.args[0]
+    assert (
+        err_msg == "Length of requested isotope ratios does not match length of "
+        "provided values."
+    )
 
 
 def test_iso_int_norm_unlawful(ini_default):
