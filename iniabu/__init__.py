fbc0d51fa536ec3a43947a27293dd121c3d49add
Reto Trappitsch <reto@galactic-forensics.space>
2023-08-29 16:38:41 +0200

    fix lint

--- iniabu/__init__.py
+++ iniabu/__init__.py
@@ -1,12 +1,14 @@
 """Solar System initial abundance package."""
 
 # import the standard module as ini
-from .main import IniAbu
 from ._version import __version__
+from .main import IniAbu
 
 ini = IniAbu()
 inimf = IniAbu(unit="mass_fraction")
 inilog = IniAbu(unit="num_log")
+
+__all__ = ["ini", "inimf", "inilog", "__version__"]
 
 __title__ = "iniabu"
 __description__ = (
