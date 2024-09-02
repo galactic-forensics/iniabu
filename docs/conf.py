f9c5be8a7a675d35c83ae8d631ba7aac9b406d0f
Reto Trappitsch <reto@galactic-forensics.space>
2024-09-02 21:51:02 +0200

    rtd theme for docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = importlib.metadata.version("iniabu")
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ["_templates"]
@@ -38,7 +38,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
